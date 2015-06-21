8d434eba1a44a350f5ce39d049be31313000a002
godzirrasti <kevinhalliwell@Kevins-MacBook-Air.local>
2015-06-20 20:17:17 -0700

    Original Code
    Orignal program…. Before I made new changes. Git Starter

---
+++ pressure/pressure.ino
@@ -0,0 +1,31 @@
+int Senval=0;
+
+int Senpin=A0;
+
+void setup()
+{
+    Serial.begin(9600);
+    pinMode(10, OUTPUT); //Red Led
+    pinMode(11, OUTPUT); //Green Led
+    pinMode(12, OUTPUT); //Buzzer
+}
+
+void loop()
+{
+
+    Senval=analogRead(Senpin);
+    Senval -= 115;
+    Serial.println(Senval);
+    delay(200);
+    
+    if (Senval >= 200) //Adjust for weight reading
+    {
+    digitalWrite(11, LOW); //Green Led
+    digitalWrite(10, HIGH); //Red Led
+    }
+    else
+    {
+    digitalWrite(11, HIGH); //Green Led
+    digitalWrite(10, LOW); //Red Led
+    }    
+}
