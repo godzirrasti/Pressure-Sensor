a25a7c86b49fee4dd99cee0cc8b372ca13d5ef1e
godzirrasti <kevinhalliwell@Kevins-MacBook-Pro.local>
2016-04-27 18:32:33 -0700

    Sync
    Sync

--- pressure/pressure.ino
+++ pressure/pressure.ino
@@ -1,18 +1,20 @@
 //Libraries
-#include <SPI.h>
 #include <SD.h>
+#include <SPI.h>
 
 //Define pin values.
-#define redLedPin 11 //Red LED
-#define greenLedPin 12 //Green LED
-#define buzzerPin 13 //Buzzer
+#define redLedPin 2 //Red LED
+#define greenLedPin 3 //Green LED
+#define buzzerPin 4 //Buzzer
 #define weightValue 100 //Adjusted so sensor does not read randomly
 #define countTo 20 //Time count
 int Senval=0; //Sensor Value
 int Senpin=A0; //Sensor read pin
 int i = 0; //Counter
-const int chipSelect = 4;
-
+int chipSelect = 10;
+int pow_pin = 8;
+String dataString = "";
+File dataFile;
 
 //Yellow wire is +5v, Red wire is read pin A0, Black wire is ground
 //Led lights use the same ground. 
@@ -30,48 +32,44 @@ void setup()
     pinMode(redLedPin, OUTPUT); //Red Led
     pinMode(greenLedPin, OUTPUT); //Green Led
     pinMode(buzzerPin, OUTPUT); //Buzzer
-
-    while (!Serial) {
-      ; //Wait for serial port.
-    }
-
+    pinMode(chipSelect, OUTPUT); //Chip select
+    pinMode(pow_pin, OUTPUT);
+    digitalWrite(pow_pin, HIGH);
     Serial.print("Initializing SD card..");
     if (!SD.begin(chipSelect)){
       Serial.println("SD card not present or failed to load");
       return;
     }
-
     Serial.println("SD card initialized");
 }
 
 void loop()
 {
     //Start string for data log
-    String dataString = "";
+    //String dataString = "";
 
     //Assign sensor readings to a variable
     Senval=analogRead(Senpin);
 
-    //Read variable and write to string
-    //Only one pin will be read, change middle value for more pins.
-    for (int analogPin = 0; analogPin < 1; analogPin++) {
-      int sensor = analogRead(analogPin);
-      dataString += String(sensor);
-    }
-
     //Open the file
-    File dataFile = SD.open("datalog.txt", FILE_WRITE);
+    dataFile = SD.open("Datalog.txt", FILE_WRITE);
+    int dataInt = (Senval);
     
     //Print if the file is available
     if (dataFile) {
-      dataFile.println(dataString);
+      dataFile.println(dataInt);
       dataFile.close();
-      Serial.println(dataString);
+      Serial.println("File successfully written!");
+    }
+    else {
+      Serial.println("Error opening file");
     }
 
     //Original print for testing
-    //Serial.println(Senval); //Print resistance
-    Serial.println(i); //Print time value
+    Serial.println("R value:"); //Print resistance
+    Serial.println(Senval);
+    Serial.println("Time: "); //Print time value
+    Serial.println(i);
     delay(200);
     i+=1; //Sets buzzer counter
     
@@ -80,21 +78,21 @@ void loop()
       digitalWrite(greenLedPin, LOW); //Green LED
       digitalWrite(redLedPin, HIGH); //Red LED
       digitalWrite(buzzerPin, LOW); //Turns buzzer input to OFF
-      dataString += "Red LED set to ON, Sensor: " + Senval;
+      //dataString += "Red LED set to ON, Sensor: " + dataInt;
     }
     else if (Senval >= weightValue && i >= countTo)
     {
       digitalWrite(buzzerPin, HIGH); //Sounds Buzzer after 2 hours
       digitalWrite(redLedPin, HIGH); //Red LED
       digitalWrite(greenLedPin, LOW); //Green LED
-      dataString += "Green LED set to ON, Sensor: " + Senval;
+      //dataString += "Green LED set to ON, Sensor: " + dataInt;
     }
     else
     {
       digitalWrite(greenLedPin, HIGH); //Green LED
       digitalWrite(redLedPin, LOW); //Red LED
       digitalWrite(buzzerPin, LOW); //Turns buzzer input to OFF
-      dataString += "Green LED set to ON, Sensor: " + Senval;
+      //dataString += "Green LED set to ON, Sensor: " + dataInt;
       i = 0; //Resets buzzer counter
     }
     
